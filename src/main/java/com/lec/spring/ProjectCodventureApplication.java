fca139386f57193f12661eee03e0841048f01230
qkrtjdals917 <qkrtjdals917@gmail.com>
2021-09-10 13:33:58 +0900

    #1 베이스 Spring 서버 세팅

---
+++ src/main/java/com/lec/spring/ProjectCodventureApplication.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCodventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCodventureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lec/spring/ProjectCodventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCodventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
