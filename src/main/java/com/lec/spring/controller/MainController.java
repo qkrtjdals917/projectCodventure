33a24d9409bfab169bb947520b2d4b0aca54f148
Quedo999 <sri00119@naver.com>
2021-10-02 20:07:52 +0900

    # 권혁찬 pull 당기기전 commit

--- src/main/java/com/lec/spring/controller/MainController.java
+++ src/main/java/com/lec/spring/controller/MainController.java
@@ -13,12 +13,12 @@ import com.lec.spring.service.UserService;
 @RequestMapping("/modacon")
 public class MainController {
 	private UserService userService;
-	
+
 	@Autowired
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
-	
+
 	@RequestMapping("")
 	public String mainPage(Model model, Principal principal) {
 //		model.addAttribute("key","service.__()");
@@ -27,53 +27,67 @@ public class MainController {
 			System.out.println("타입정보 : " + principal.getClass());
 			System.out.println("ID정보 : " + principal.getName());
 		}
-		
+
 		return "user/main";
 	}
-	
-	//코인, 
+
+	// 코인,
 	@RequestMapping("/coin")
 	public String coin() {
-		
+
 		return "user/coin_main";
 	}
-	//커뮤니티, 
+
+	// 커뮤니티 리스트
 	@RequestMapping("/board")
 	public String board(Model model) { // 여기에서 작성 부분은 로그인 한 사람만 접근가능
 		model.addAttribute("list", userService.communityList());
 		return "user/board/list";
 	}
-	
+
 	// 정보게시판 리스트
 	@RequestMapping("/infoboard")
 	public String infoBoard(Model model) {
 		model.addAttribute("list", userService.infoList());
 		return "user/board/list";
 	}
-	
+
 	// 자유게시판 리스트
 	@RequestMapping("/freeboard")
 	public String freeBoard(Model model) {
 		model.addAttribute("list", userService.freeList());
 		return "user/board/list";
 	}
-	
-	//공지
+
+	// 커뮤니티 뷰
+	@RequestMapping("/board/view")
+	public String communityView(int uid, Model model) {
+		model.addAttribute("list", userService.viewContent(uid));
+		return "user/board/view";
+	}
+
+	// 게시판 글 작성
+	@RequestMapping("/board/write")
+	public String write(Model model) {
+		return "user/board/write";
+	}
+
+	// 공지 리스트
 	@RequestMapping("/notice")
 	public String notice(Model model) {
 		model.addAttribute("list", userService.noticeList());
 		return "user/notice/list";
 	}
-	
+
 	// 공지 뷰
 	@RequestMapping("notice/view")
 	public String noticeView(int uid, Model model) {
 		model.addAttribute("list", userService.viewContent(uid));
 		return "user/notice/view";
 	}
-	
-	//-------------------여기 이하 admin---------------------
-	
+
+	// -------------------여기 이하 admin---------------------
+
 //	@GetMapping("/logincheck")
 //	@ResponseBody
 //	public String login(Authentication authentication) {
