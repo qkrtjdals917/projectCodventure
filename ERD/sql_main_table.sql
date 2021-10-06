c35aaff799ed084445cf054736961a7507928b2d
haefight <hyis1201@gmail.com>
2021-10-06 13:25:24 +0900

    커밋

--- ERD/sql_main_table.sql
+++ ERD/sql_main_table.sql
@@ -190,17 +190,12 @@ SELECT mb.board_uid board_uid, mb.subject subject, mm.nickname nickname,
 
 SELECT * FROM mc_like;
 
-<<<<<<< HEAD
-=======
 -- 글 작성
 INSERT INTO mc_board (type, subject, tag, content, member_uid)
 VALUES ("자유", "글작성테스트sql", "" ,"이 글은 테스트 중입니다.", 52);
 
 SELECT *FROM mc_board mb ;
 
-
--- -------------------------------------------------------------
->>>>>>> branch 'master' of https://github.com/qkrtjdals917/projectCodventure.git
 		SELECT	
 			member_uid "member_uid",
 			email "email",
