048556614b038441823cbc201ffb0b2f65983768
qkrtjdals917 <qkrtjdals917@gmail.com>
2021-10-12 18:03:02 +0900

    ㅁㄴㅇㄹ

--- ERD/sql_main_table.sql
+++ ERD/sql_main_table.sql
@@ -114,7 +114,7 @@ ALTER TABLE mc_like
 	ADD FOREIGN KEY (board_uid)
 	REFERENCES mc_board (board_uid)
 	ON UPDATE RESTRICT
-	ON DELETE CASCADE
+	ON DELETE RESTRICT
 ;
 
 
@@ -145,8 +145,8 @@ ALTER TABLE mc_comment
 ALTER TABLE mc_like
 	ADD FOREIGN KEY (member_uid)
 	REFERENCES mc_member (member_uid)
-	ON UPDATE CASCADE
-	ON DELETE CASCADE
+	ON UPDATE RESTRICT
+	ON DELETE RESTRICT
 ;
 
 
@@ -204,34 +204,10 @@ SELECT mb.board_uid board_uid, mb.subject subject, mm.nickname nickname,
 SELECT * FROM mc_like;
 
 -- 글 작성
-
 INSERT INTO mc_board (type, subject, tag, content, member_uid)
 
-INSERT INTO mc_board (type, subject, tag, content, member_uid)
 VALUES ("자유", "글작성테스트sql", "" ,"이 글은 테스트 중입니다.", 52);
 
-SELECT *FROM mc_board mb ;
-
-DESC mc_board ;
-
--- 추천
-SELECT member_uid FROM mc_like WHERE board_uid = 226;
-SELECT member_uid FROM mc_like WHERE board_uid = 226 AND member_uid = 1;
-
-INSERT INTO mc_like (board_uid, member_uid)
-SELECT 226, 1 FROM DUAL WHERE NOT EXISTS (SELECT board_uid FROM mc_like WHERE board_uid = 226 AND member_uid = 1);
-
-SELECT * FROM mc_like;
-
-SELECT count(member_uid) FROM mc_like WHERE board_uid = 226;
-
-DELETE FROM mc_like WHERE board_uid = 226 AND member_uid = 1;
-
--- 신고 확인
-SELECT * FROM mc_report mr ;
--- -------------------------------------------------------------
-
-SELECT *FROM mc_board mb ;
 SELECT *FROM mc_board mb ;
 		SELECT	
 			member_uid "member_uid",
